122099279093275d041a8095af03029c83c01dc2
Humpty001 <99479541+Humpty001@users.noreply.github.com>
2022-02-11 15:54:16 +0300

    Add files via upload

---
+++ area.py
@@ -0,0 +1,7 @@
+number1 = int(input("enter the first n number"))
+numbeer2= int(input("enter the second n number"))
+number3= int(input("enter the third n number"))
+
+sum_of_the_natural_number= number1+numbeer2+number3
+
+print("som of the natural number is",sum_of_the_natural_number)
